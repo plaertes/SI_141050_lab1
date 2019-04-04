8132e5ab0357ffa7c03340b467890526cf10e10f
Leart Pajaziti <leo-leart@hotmail.com>
2019-04-04 09:56:43 +0200

    functions implemented

--- program.cpp
+++ program.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 void printMyName () {
 	//TODO
+	cout<<"Leart Pajaziti\n"; 
 }
 
 void printMyIndex () {
 	//TODO
+	cout<<"141050"; 
 }
 
 int main () {
 	//TODO
+	printMyName ();
+	printMyIndex ();
+	
 }
